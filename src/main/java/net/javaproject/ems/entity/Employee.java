da6932e94dae1b919ea54bd6dbb1a0dc9cfe3e84
harshal <kardileharshal567@gmail.com>
2024-04-30 19:11:35 +0530

    initial setup

---
+++ src/main/java/net/javaproject/ems/entity/Employee.java
@@ -0,0 +1,29 @@
+package net.javaproject.ems.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firsName;
+
+    @Column( name = "last_name")
+    private String lasName;
+
+    @Column(name = "email_id", nullable = false, unique = true)
+    private String email;
+}
